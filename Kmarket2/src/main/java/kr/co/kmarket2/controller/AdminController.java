431213b1e2197487ae790a1c185090b8734afb50
구홍모 <khm9990503@gmail.com>
2023-02-10 13:34:15 +0900

    2023/02/10 admin 헤더푸터작업 완료

--- Kmarket2/src/main/java/kr/co/kmarket2/controller/AdminController.java
+++ Kmarket2/src/main/java/kr/co/kmarket2/controller/AdminController.java
@@ -1,8 +1,21 @@
 package kr.co.kmarket2.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
-
+	
+	@GetMapping("admin/index")
+	public String index() {
+		return "admin/index";
+	}
+	@GetMapping("admin/list")
+	public String list() {
+		return "admin/list";
+	}
+	@GetMapping("admin/register")
+	public String register() {
+		return "admin/register";
+	}
 }
