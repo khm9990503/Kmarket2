691137af6aa1fbfadb004e1527c73c95b5520398
haerisogyumo <haerisogyumo@gmail.com>
2023-02-10 13:34:17 +0900

    member/cs-헤더푸터 타임리프처리 , member-signup 약관가져오기 편집중

--- Kmarket2/src/main/java/kr/co/kmarket2/controller/CsController.java
+++ Kmarket2/src/main/java/kr/co/kmarket2/controller/CsController.java
@@ -1,8 +1,21 @@
 package kr.co.kmarket2.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CsController {
 
+	@GetMapping("cs/index")
+	public String index() {
+		return "cs/index";
+	}
+	
+	@GetMapping("cs/qna/list")
+	public String qnaList() {
+		return "cs/qna/list";
+	}
+	
+	
+	
 }
--- Kmarket2/src/main/java/kr/co/kmarket2/controller/MemberController.java
+++ Kmarket2/src/main/java/kr/co/kmarket2/controller/MemberController.java
@@ -1,8 +1,51 @@
 package kr.co.kmarket2.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import kr.co.kmarket2.service.MemberService;
+import kr.co.kmarket2.vo.TermsVO;
 
 @Controller
 public class MemberController {
+	
+	@Autowired
+	private MemberService service;
+	
+	@GetMapping("member/join")
+	public String join() {
+		return "member/join";
+	}
+	
+	@GetMapping("member/login")
+	public String login() {
+		return "member/login";
+	}
+	
+	@GetMapping("member/register")
+	public String register() {
+		return "member/register";
+	}
+	
+	@GetMapping("member/registerSeller")
+	public String registerSeller() {
+		return "member/registerSeller";
+	}
+	
+	@GetMapping("member/signup")
+	public String signup(Model model) {
+		 TermsVO vo = service.selectTerms();
+		 model.addAttribute("vo", vo);
+		return "member/signup";
+	}
+	
+	
+	 
+	
+	
+	
 
+	
 }
