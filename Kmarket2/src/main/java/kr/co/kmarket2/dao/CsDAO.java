e7fe2ec21aa1b83c52863b97bd1692bba687ab5c
sorkcoavus <sorkcoavus@gmail.com>
2023-02-15 13:35:37 +0900

    faq list/view완료 더보기작업중

--- Kmarket2/src/main/java/kr/co/kmarket2/dao/CsDAO.java
+++ Kmarket2/src/main/java/kr/co/kmarket2/dao/CsDAO.java
@@ -12,16 +12,32 @@ import kr.co.kmarket2.vo.ArticleVO;
 @Repository
 public interface CsDAO {
 	
+	// notice list
 	public List<ArticleVO> selectArticles(String group, int start);
 	
 	public List<ArticleVO> selectCateArticles(String group, String cate, int start);
 	
-	// 리스트 페이징
+	
+	// list paging
 	public int selectCountTotal(String group);
 	
 	// 카테고리별 리스트 페이징
 	public int selectCountCateTotal(String group, String cate);
 	
+	// view
 	public ArticleVO selectArticle(int no);
+	
+	
+	//faq list
+	public List<ArticleVO> selectFaqArticles(String group, String cate);
+	
+	//faq category
+	public List<ArticleVO> selectFaqCates(String cate);
+	
+	//index notice list
+	public List<ArticleVO> selectNotices(String group);
+	
+	
+	
 
 }
