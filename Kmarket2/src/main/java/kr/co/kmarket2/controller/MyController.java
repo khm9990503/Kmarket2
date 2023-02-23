76e367f289aa516b43a3074b13f2fa02e0274265
sorkcoavus <sorkcoavus@gmail.com>
2023-02-23 13:35:18 +0900

    my qna 작업중

--- Kmarket2/src/main/java/kr/co/kmarket2/controller/MyController.java
+++ Kmarket2/src/main/java/kr/co/kmarket2/controller/MyController.java
@@ -1,11 +1,21 @@
 package kr.co.kmarket2.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import kr.co.kmarket2.service.MyService;
+import kr.co.kmarket2.vo.ArticleVO;
 
 @Controller
 public class MyController {
 
+	@Autowired
+	private MyService service;
+	
 	@GetMapping("my/home")
 	public String home() {
 		return "my/home";
@@ -27,7 +37,33 @@ public class MyController {
 		return "my/info";
 	}
 	@GetMapping("my/qna")
-	public String qna() {
+	public String qna(String group,String uid, String pg ,Model model) {
+		
+		//페이징 
+    	int currentPage = service.getCurrentPage(pg); // 현재 페이지 번호
+		int total = 0;
+		
+		
+		total = service.selectCountTotal(group); //전체 게시물 갯수
+		
+			
+		
+		int lastPageNum = service.getLastPageNum(total);// 마지막 페이지 번호
+		int[] result = service.getPageGroupNum(currentPage, lastPageNum); // 페이지 그룹번호
+		int pageStartNum = service.getPageStartNum(total, currentPage); // 페이지 시작번호
+		int start = service.getStartNum(currentPage); // 시작 인덱스
+		
+		model.addAttribute("lastPageNum", lastPageNum);		
+		model.addAttribute("currentPage", currentPage);		
+		model.addAttribute("pageGroupStart", result[0]);
+		model.addAttribute("pageGroupEnd", result[1]);
+		model.addAttribute("pageStartNum", pageStartNum+1);
+		
+		// qna 목록(c1Name join) 가져오기
+		List<ArticleVO> articles = service.selectQnaArticles(uid, start);
+		
+		model.addAttribute("articles", articles);
+		
 		return "my/qna";
 	}
 }
--- Kmarket2/src/main/java/kr/co/kmarket2/dao/MyDAO.java
+++ Kmarket2/src/main/java/kr/co/kmarket2/dao/MyDAO.java
@@ -1,8 +1,19 @@
 package kr.co.kmarket2.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import kr.co.kmarket2.vo.ArticleVO;
+
+@Mapper
 @Repository
 public interface MyDAO {
-
+	
+	// 문의하기 리스트
+	public List<ArticleVO> selectQnaArticles(String uid, int start);
+	
+	// 문의하기 페이징(총 갯수)
+	public int selectCountTotal(String group);
 }
